74e453e9082da4cf390c33d25ba1fb97d77b8438
wld1008 <1175173151@qq.com>
2019-05-07 18:59:16 +0800

    Update ImommodityWarehouseController.java
    ImommodityWarehouse

--- src/main/java/com/pastry/controller/ImommodityWarehouseController.java
+++ src/main/java/com/pastry/controller/ImommodityWarehouseController.java
@@ -3,5 +3,20 @@ package com.pastry.controller;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Controller
+@RequestMapping("/imommodityWarehouse")
 public class ImommodityWarehouseController {
+  @Resource
+  private ImommodityWarehouseService imommodityWarehouseService;
+  
+  @RequestMapping("/list")
+  public String queryList(HttpServletRequest request, HttpServletResponse response) {
+      return "/imommodityWarehouse/list";
+  }
+  
+   @ResponseBody
+   @RequestMapping("/listData")
+   public PageResult<List<ImommodityWarehouse>> list(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+      return imommodityWarehouseService.getAll(page, limit);
+   }
 }
