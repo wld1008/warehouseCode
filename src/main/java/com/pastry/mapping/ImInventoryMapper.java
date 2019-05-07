c24574622f449f1a1f8e57f14fe3612a6c805f2a
wld1008 <1175173151@qq.com>
2019-05-07 20:07:23 +0800

    Update ImInventoryMapper.java
    ImInventory

--- src/main/java/com/pastry/mapping/ImInventoryMapper.java
+++ src/main/java/com/pastry/mapping/ImInventoryMapper.java
@@ -14,4 +14,20 @@ public interface ImInventoryMapper {
     int updateByPrimaryKeySelective(ImInventory record);
 
     int updateByPrimaryKey(ImInventory record);
+    
+    
+    List<ImInventory> getAll(@Param("start") int start, @Param("limit") int limit);
+
+    /**
+     * 根据ID删除数据
+     * @param ids
+     * @return
+     */
+    int deleteBatch(@Param("ids") List<Integer> ids);
+
+    /**
+     * 查询总数
+     * @return
+     */
+    int getCount();
 }
