18cb97d4e5e6befd151613e91d0e0b8d59d2af9a
wld1008 <1175173151@qq.com>
2019-05-07 19:54:01 +0800

    Update ImommodityWarehouseService.java
    ImommodityWarehouse

--- src/main/java/com/pastry/service/ImommodityWarehouseService.java
+++ src/main/java/com/pastry/service/ImommodityWarehouseService.java
@@ -4,4 +4,16 @@ package com.pastry.service;
  * Created by wulidan on 19/5/7.
  */
 public interface ImommodityWarehouseService {
+   /**
+     * 查询所有信息
+     * @return
+     */
+    PageResult<List<ImommodityWarehouse>> getAll(int page, int limit);
+
+    /**
+     * 根据ID批量删除
+     * @param ids
+     * @return
+     */
+    PageResult deleteBatch(List<Integer> ids);
 }
