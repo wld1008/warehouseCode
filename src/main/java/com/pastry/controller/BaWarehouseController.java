7fc0a2c0d2eaa3c3812f62f7638514a9db1008f2
wld1008 <1175173151@qq.com>
2019-05-07 16:07:33 +0800

    Update BaWarehouseController.java
    新增warehouse Controller方法

--- src/main/java/com/pastry/controller/BaWarehouseController.java
+++ src/main/java/com/pastry/controller/BaWarehouseController.java
@@ -3,5 +3,22 @@ package com.pastry.controller;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Controller
+@RequestMapping("/baWarehouse")
 public class BaWarehouseController {
+  
+  @Resource
+  private BaWarehouseService baWarehouseService;
+  
+  @RequestMapping("/list")
+  public String queryList(HttpServletRequest request, HttpServletResponse response) {
+      return "/warehouse/list";
+  }
+  
+   @ResponseBody
+   @RequestMapping("/listData")
+   public PageResult<List<BaShopDO>> list(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+      return baWarehouseService.getAll(page, limit);
+   }
+  
 }
