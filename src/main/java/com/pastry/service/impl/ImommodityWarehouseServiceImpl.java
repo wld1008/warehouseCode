665d5ead5c087709a73e61eddf176fd079086324
wld1008 <1175173151@qq.com>
2019-05-07 20:00:02 +0800

    Update ImommodityWarehouseServiceImpl.java
    imommodityWarehouse

--- src/main/java/com/pastry/service/impl/ImommodityWarehouseServiceImpl.java
+++ src/main/java/com/pastry/service/impl/ImommodityWarehouseServiceImpl.java
@@ -3,5 +3,29 @@ package com.pastry.service.impl;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Service
 public class ImommodityWarehouseServiceImpl {
+  
+    @Resource
+    private ImommodityWarehouseMapper imommodityWarehouseMapper;
+
+    @Override
+    public PageResult<List<BaShopDO>> getAll(int page, int limit) {
+        PageResult pageResult = new PageResult();
+        int startRow = (page - 1) * limit;
+        pageResult.setData(imommodityWarehouseMapper.getAll(startRow, limit));
+        pageResult.setCount(imommodityWarehouseMapper.getCount());
+        return pageResult;
+    }
+
+    @Override
+    public PageResult deleteBatch(List<Integer> ids) {
+        PageResult pageResult = new PageResult();
+        int val = imommodityWarehouseMapper.deleteBatch(ids);
+        if(val == 0) {
+            pageResult.setCode(1001);
+            pageResult.setMsg("没有数据");
+        }
+        return pageResult;
+    }
 }
