e085a61ab02920a654c9308f8ed0b9df1c1b81b8
wld1008 <1175173151@qq.com>
2019-05-07 18:55:17 +0800

    Update ImInventoryController.java
    物流跟踪

--- src/main/java/com/pastry/controller/ImInventoryController.java
+++ src/main/java/com/pastry/controller/ImInventoryController.java
@@ -3,5 +3,20 @@ package com.pastry.controller;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Controller
+@RequestMapping("/inventory")
 public class ImInventoryController {
+  @Resource
+  private ImInventoryService imInventoryService;
+  
+  @RequestMapping("/list")
+  public String queryList(HttpServletRequest request, HttpServletResponse response) {
+      return "/delivery/list";
+  }
+  
+   @ResponseBody
+   @RequestMapping("/listData")
+   public PageResult<List<ImInventory>> list(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+      return imInventoryService.getAll(page, limit);
+   }
 }
