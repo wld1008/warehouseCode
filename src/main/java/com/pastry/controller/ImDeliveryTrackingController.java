e3913d1a37f741cf42925b24b30e11839ffc0936
wld1008 <1175173151@qq.com>
2019-05-07 16:53:49 +0800

    Update ImDeliveryTrackingController.java
    物流跟踪

--- src/main/java/com/pastry/controller/ImDeliveryTrackingController.java
+++ src/main/java/com/pastry/controller/ImDeliveryTrackingController.java
@@ -3,5 +3,21 @@ package com.pastry.controller;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Controller
+@RequestMapping("/deliveryTrack")
 public class ImDeliveryTrackingController {
+  
+  @Resource
+  private ImDeliveryTracking deliveryTracking;
+  
+  @RequestMapping("/list")
+  public String queryList(HttpServletRequest request, HttpServletResponse response) {
+      return "/delivery/list";
+  }
+  
+   @ResponseBody
+   @RequestMapping("/listData")
+   public PageResult<List<ImDeliveryTracking>> list(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+      return deliveryTracking.getAll(page, limit);
+   }
 }
