bfee5d40a565cf62de1c4849ac757b51ab33cc1f
wld1008 <1175173151@qq.com>
2019-05-07 20:05:13 +0800

    Update BaWarehouseMapper.java
    BaWarehouse

--- src/main/java/com/pastry/mapping/BaWarehouseMapper.java
+++ src/main/java/com/pastry/mapping/BaWarehouseMapper.java
@@ -14,4 +14,20 @@ public interface BaWarehouseMapper {
     int updateByPrimaryKeySelective(BaWarehouse record);
 
     int updateByPrimaryKey(BaWarehouse record);
+    
+    List<BaWarehouse> getAll(@Param("start") int start, @Param("limit") int limit);
+
+    /**
+     * 根据ID删除数据
+     * @param ids
+     * @return
+     */
+    int deleteBatch(@Param("ids") List<Integer> ids);
+
+    /**
+     * 查询总数
+     * @return
+     */
+    int getCount();
+
 }
