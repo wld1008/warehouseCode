6c2e60f401f7791a288aca0b20d21a2a88b15cf4
wld1008 <1175173151@qq.com>
2019-05-07 20:08:30 +0800

    Update ImommodityWarehouseMapper.java
    ImommodityWarehouse

--- src/main/java/com/pastry/mapping/ImommodityWarehouseMapper.java
+++ src/main/java/com/pastry/mapping/ImommodityWarehouseMapper.java
@@ -14,4 +14,19 @@ public interface ImommodityWarehouseMapper {
     int updateByPrimaryKeySelective(ImommodityWarehouse record);
 
     int updateByPrimaryKey(ImommodityWarehouse record);
+    
+     List<ImommodityWarehouse> getAll(@Param("start") int start, @Param("limit") int limit);
+
+    /**
+     * 根据ID删除数据
+     * @param ids
+     * @return
+     */
+    int deleteBatch(@Param("ids") List<Integer> ids);
+
+    /**
+     * 查询总数
+     * @return
+     */
+    int getCount();
 }
