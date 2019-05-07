152ca2929013d9045b05a968b8db68485233f962
wld1008 <1175173151@qq.com>
2019-05-07 19:58:58 +0800

    Update ImInventoryServiceImpl.java
    imInventory

--- src/main/java/com/pastry/service/impl/ImInventoryServiceImpl.java
+++ src/main/java/com/pastry/service/impl/ImInventoryServiceImpl.java
@@ -3,5 +3,29 @@ package com.pastry.service.impl;
 /**
  * Created by wulidan on 19/5/7.
  */
+@Service
 public class ImInventoryServiceImpl {
+  
+    @Resource
+    private ImInventoryMapper imInventoryMapper;
+
+    @Override
+    public PageResult<List<ImInventory>> getAll(int page, int limit) {
+        PageResult pageResult = new PageResult();
+        int startRow = (page - 1) * limit;
+        pageResult.setData(imInventoryMapper.getAll(startRow, limit));
+        pageResult.setCount(imInventoryMapper.getCount());
+        return pageResult;
+    }
+
+    @Override
+    public PageResult deleteBatch(List<Integer> ids) {
+        PageResult pageResult = new PageResult();
+        int val = imInventoryMapper.deleteBatch(ids);
+        if(val == 0) {
+            pageResult.setCode(1001);
+            pageResult.setMsg("没有数据");
+        }
+        return pageResult;
+    }
 }
